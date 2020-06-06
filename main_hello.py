5878329d581a3d121a42a464d93748d91c14d681
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2020-06-06 20:39:37 -0300

    first commit

---
+++ main_hello.py
@@ -0,0 +1,9 @@
+def main():
+    print("python main function")
+
+    print("Hello")
+
+    print("__name__ value = ", __name__)# __name__ is equal '__main__'
+
+if __name__ == '__main__':
+    main()#call main function
